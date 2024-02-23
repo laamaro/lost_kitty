e5430b7d8d47534614ae8613f21704d773487723
Laura Amaro <lauraamaro1804@gmail.com>
2024-02-22 22:20:54 -0300

    finish livecode

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,50 @@
 class PetsController < ApplicationController
+  before_action :set_pet, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @pets = Pet.all
+  end
+
+  def show
+  end
+
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+    if @pet.save
+      redirect_to pet_path(@pet)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    @pet.update(pet_params)
+
+    redirect_to pet_path(@pet)
+  end
+
+  def destroy
+
+    @pet.destroy
+
+    redirect_to pets_path
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :species, :age, :size, :found_on)
+  end
+
+  def set_pet
+    @pet = Pet.find(params[:id])
+  end
 end
